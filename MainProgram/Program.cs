7cc696efa6b03cc60f0aad687fae9eab916cb1da
Arael Seher <a.seher.85@gmail.com>
2025-05-05 21:47:56 -0700

    created program that checks CSV file to find 2 furthest apart locations. Want to add logging functionality

--- MainProgram/Program.cs
+++ MainProgram/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Device.Location;
 
-Console.WriteLine("Hello, World!");
+using MainProgram;
+
+var line = File.ReadAllLines("TacoBell-US-AL.csv");
+var tacoBell =  line.Select(item =>Parse.ParseCSV(item)).ToArray();//@TODO study up on .Select()
+
+double distance = 0;
+var farthestA = "";
+var farthestB = "";
+
+for (int i = 0; i < line.Length; i++)
+{
+    var tacoBellA = tacoBell[i];
+    var geoTacoBellA = new GeoCoordinate(tacoBellA.Location.Latitude, tacoBellA.Location.Longitude);
+    for (int j = i+1; j < line.Length; j++)
+    {
+        var tacoBellB = tacoBell[j];
+        var geoTacoBellB = new GeoCoordinate(tacoBellB.Location.Latitude, tacoBellB.Location.Longitude);
+        
+        if (geoTacoBellA.GetDistanceTo(geoTacoBellB) > distance)
+        {
+            distance = geoTacoBellA.GetDistanceTo(geoTacoBellB);
+            farthestA = tacoBellA.Name;
+            farthestB = tacoBellB.Name;
+        }
+    }
+}
+
+Console.WriteLine($"The two farthest Taco Bells are {farthestA} and {farthestB}. They are {distance:F2} meters apart.");
+Console.WriteLine($"The distance in miles is {distance/1609.34:F2} miles.");
